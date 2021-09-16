1c26710e1c097afd98dee13f6760565d42d58724
regantithejesh <52595774+regantithejesh@users.noreply.github.com>
2021-09-16 17:29:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,25 @@
 variable "subnet_id" {
   description = "The AWS region to create things in."
-  default     = "sub-12345"
+  type = string
+  default     = "subnet-00df62d333354c0ce"
 }
 variable "ami_id" {
+  type = string
   description = "specify amiid"
-  default     = "ami=12345"
+  default     = "ami-0c2b8ca1dad447f8a"
 }
 variable "instance_region" {
+  type = string
   description = "specify region"
   default     = "us-east-1"
 }
 variable "instance_type" {
+  type = string
   description = "specify instance type"
   default     = "t2.micro"
+}
+variable "instance_vpcid" {
+  type = string
+  description = "specify vpc id"
+  default     = "vpc-011c048571ccc118a"
 }
